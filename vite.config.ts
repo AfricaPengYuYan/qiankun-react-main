087d5ce6870653442dc6778c0616546d4cbfc48c
luo <2731800406@qq.com>
2024-08-20 10:43:38 +0800

    feat:init

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,46 @@
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import { wrapperEnv } from './src/utils/wrapperEnv';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        react()
-    ],
-    server: {
-        port: 13000
-    },
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "./src")
+export default defineConfig((mode) => {
+    const env = loadEnv(mode.mode, process.cwd());
+    const viteEnv = wrapperEnv(env);
+
+    return {
+        plugins: [
+            react()
+        ],
+        server: {
+            host: "0.0.0.0",
+            port: viteEnv.VITE_PORT,
+            open: viteEnv.VITE_OPEN,
+            cors: true,
+            proxy: {
+
+            }
+        },
+        css: {
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true,
+                    additionalData: `@import "@/assets/styles/index.less";`
+                }
+            }
+        },
+        esbuild: {
+            pure: viteEnv.VITE_DROP_CONSOLE ? ["console.log", "debugger"] : [],
+        },
+        build: {
+            outDir: "dist",
+            minify: "esbuild",
+        },
+        resolve: {
+            alias: {
+                "@": resolve(__dirname, "./src"),
+                "#": resolve(__dirname, "./src/types")
+            }
         }
     }
 })
