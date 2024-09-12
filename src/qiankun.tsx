eb8106217311a0427fd89f1f19284efc3dbc209b
luo <2731800406@qq.com>
2024-09-12 14:53:10 +0800

    refactor(qiankun): 添加应用生命周期钩子和全局异常处理器
    在qiankun配置中引入beforeLoad和beforeMount钩子，以增强子应用的调试能力和生命周期管理。同时，新增全局未捕获异常处理器，以提高应用的健壮性和错误处理能力。

--- src/qiankun.tsx
+++ src/qiankun.tsx
@@ -1,4 +1,4 @@
-import { registerMicroApps, start } from 'qiankun';
+import { addGlobalUncaughtErrorHandler, registerMicroApps, start } from 'qiankun';
 
 // 微前端 - 子应用列表
 export const APPS = [
@@ -10,7 +10,21 @@ export const APPS = [
     },
 ];
 
-registerMicroApps(APPS);
+registerMicroApps(APPS, {
+    beforeLoad: (app) => {
+        console.log('before load', app.name);
+        return Promise.resolve();
+    },
+    beforeMount: (app) => {
+        console.log('before mount', app.name);
+        return Promise.resolve();
+    }
+});
+
+// 全局的未捕获异常处理器
+addGlobalUncaughtErrorHandler((event) => {
+    console.error(event)
+});
 
 //启动 qiankun
 start({
