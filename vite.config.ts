e84d069e2b9812915c5295db24f55e6ded50c98b
luo <2731800406@qq.com>
2024-09-14 14:59:53 +0800

    refactor(vite.config): 简化环境加载函数的参数
    更新了`vite.config.ts`中的`defineConfig`调用，通过使用解构来简化环境加载函数的参数。这提高了代码的清晰度并确保了环境变量的正确加载。

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig, loadEnv } from 'vite';
 import { wrapperEnv } from './src/utils/wrapperEnv';
 
 // https://vitejs.dev/config/
-export default defineConfig((mode) => {
-    const env = loadEnv(mode.mode, process.cwd());
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
     const viteEnv = wrapperEnv(env);
 
     return {
